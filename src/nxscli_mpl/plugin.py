9edae1a85bcde44d3f72c5603975e20f3f1001e3
raiden00pl <raiden00@railab.me>
2023-02-25 12:49:39 +0100

    move cmd_mpl to config

--- src/nxscli_mpl/plugin.py
+++ src/nxscli_mpl/plugin.py
@@ -5,7 +5,7 @@ from nxscli.iplugin import DPluginDescription
 from nxscli_mpl.commands.cmd_ani1 import cmd_pani1
 from nxscli_mpl.commands.cmd_ani2 import cmd_pani2
 from nxscli_mpl.commands.cmd_cap import cmd_pcap
-from nxscli_mpl.commands.cmd_mpl import cmd_mpl
+from nxscli_mpl.commands.config.cmd_mpl import cmd_mpl
 from nxscli_mpl.plugins.animation1 import PluginAnimation1
 from nxscli_mpl.plugins.animation2 import PluginAnimation2
 from nxscli_mpl.plugins.capture import PluginCapture
